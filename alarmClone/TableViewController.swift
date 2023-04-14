079a294e53ab53de5f0f9d3bb30a35bafbd014f6
JackyQLCai <JackyQLCai@mobiledrivetech.com>
2023-04-14 15:08:11 +0800

    Try to add the UILabel in TableVeiwCell

---
+++ alarmClone/TableViewController.swift
@@ -0,0 +1,144 @@
+//
+//  TableViewController.swift
+//  alarmClone
+//
+//  Created by Cai JackyQL on 2023/4/11.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        // Register custom cell class to table view
+        self.tableView.register(timeZoneListTableViewCell.self, forCellReuseIdentifier: "TimeZoneListTableViewCell")
+        
+        // Automatically calculate row height
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = UITableView.automaticDimension
+    }
+
+
+    
+    // MARK: - Table view data source
+    class timeZoneListTableViewCell: UITableViewCell{
+        
+        @IBOutlet weak var region: UILabel!
+        
+        //@IBOutlet weak var time: UILabel!
+        
+        //@IBOutlet weak var time_equation: UILabel!
+        
+    }
+    
+    let timeZoneList = ["America/New_York", "America/Chicago", "America/Denver", "America/Los_Angeles", "America/Phoenix", "Europe/London", "Europe/Paris", "Europe/Moscow", "Asia/Tokyo", "Asia/Shanghai", "Australia/Sydney", "Pacific/Auckland", "Africa/Cairo", "Asia/Dubai", "America/Sao_Paulo", "Asia/Kolkata", "America/Toronto", "Asia/Seoul", "Europe/Stockholm", "Pacific/Honolulu"]
+
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return timeZoneList.count
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "TimeZoneListTableViewCell", for: indexPath) as? timeZoneListTableViewCell else {
+            fatalError("Unable to dequeue a TimeZoneListTableViewCell")
+        }
+        
+        // Configure the cell...
+        cell.region?.text = timeZoneList[indexPath.row]
+
+        return cell
+    }
+    
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    
+    /* --------------------------------------------------------------------------------
+     Example for table view list
+    var colors = ["red", "blue", "green", "yellow", "purple", "orange", "pink", "brown", "gray", "teal", "magenta", "lime", "olive", "navy", "maroon", "aqua", "fuchsia", "silver", "black", "white"]
+     
+     
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return colors.count
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let cellIdentifier = "Cell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+
+        // Configure the cell...
+        cell.textLabel?.text = colors[indexPath.row]
+
+        return cell
+    }
+    */
+    
+
+}
